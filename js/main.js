9e06c818392bf4af1899b0a05b8ed1912572a5d6
xxxDarkhanxxx <63459203+xxxDarkhanxxx@users.noreply.github.com>
2020-06-20 17:30:48 +0600

    feat: added something i forgot

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /* Переменая для создания ID */
-const generateId = () => `id${Math.round(Math.random() * 1e8).toString(10)}`
+const generateId = () => `id${Math.round(Math.random() * 1e8).toString(10)}`;
 
 /* Переменные */
 const totalMoneyIncome = document.querySelector(".total__money-income"),
@@ -11,16 +11,12 @@ const totalMoneyIncome = document.querySelector(".total__money-income"),
   operationName = document.querySelector(".operation__name");
 
 /* База данных и сохранение их, позже обрабатывает в init() */
-let dbOperation = JSON.parse(localStorage.getItem('calc')) || [];
-
-
+let dbOperation = JSON.parse(localStorage.getItem("calc")) || [];
 
 /* Рендер массива */
 const renderOperation = (operation) => {
-
-  const className = operation.amount < 0 ?
-    'history__item-minus' :
-    'history__item-plus';
+  const className =
+    operation.amount < 0 ? "history__item-minus" : "history__item-plus";
 
   const listItem = document.createElement("li");
 
@@ -40,30 +36,26 @@ const updateBalance = () => {
     .filter((item) => item.amount > 0)
     .reduce((sum, item) => sum + item.amount, 0);
 
-
   const resultExpenses = dbOperation
     .filter((item) => item.amount < 0)
     .reduce((sum, item) => sum + item.amount, 0);
 
-  totalMoneyIncome.textContent = resultIncome + ' ₸';
-  totalMoneyExpenses.textContent = resultExpenses + ' ₸';
-  totalBalance.textContent = (resultIncome + resultExpenses) + ' ₸';
-
+  totalMoneyIncome.textContent = resultIncome + " ₸";
+  totalMoneyExpenses.textContent = resultExpenses + " ₸";
+  totalBalance.textContent = resultIncome + resultExpenses + " ₸";
 };
 
 /* Добавление строки и цвет этой таблички с borderom*/
-
 const addOperation = (event) => {
   event.preventDefault();
 
   const operationNameValue = operationName.value,
     operationAmountValue = operationAmount.value;
 
-  operationName.style.borderColor = '';
-  operationAmount.style.borderColor = '';
+  operationName.style.borderColor = "";
+  operationAmount.style.borderColor = "";
 
   if (operationNameValue && operationAmountValue) {
-
     const operation = {
       id: generateId(),
       description: operationNameValue,
@@ -73,44 +65,38 @@ const addOperation = (event) => {
     dbOperation.push(operation);
     init();
     console.log(dbOperation);
-
-
   } else {
-    if (!operationNameValue) operationName.style.borderColor = 'red';
-    if (!operationNameValue) operationAmount.style.borderColor = 'red';
+    if (!operationNameValue) operationName.style.borderColor = "red";
+    if (!operationNameValue) operationAmount.style.borderColor = "red";
   }
 
-  operationName.value = '';
-  operationAmount.value = '';
+  operationName.value = "";
+  operationAmount.value = "";
 };
 
 /* Функция для удаления строк c помощью делегирования */
 const deleteOperation = (event) => {
-
-  if (event.target.classList.contains('history_delete')) {
+  if (event.target.classList.contains("history_delete")) {
     const target = event.target;
-    dbOperation = dbOperation
-      .filter(operation => operation.id !== target.dataset.id);
+    dbOperation = dbOperation.filter(
+      (operation) => operation.id !== target.dataset.id
+    );
 
     init();
   }
-
-
 };
-
 
 /* Запускает функцию с рендером в цикле и обновляет счетчик с деньгами*/
 
 const init = () => {
   historyList.textContent = "";
-  dbOperation.forEach(renderOperation)
+  dbOperation.forEach(renderOperation);
   updateBalance();
-  localStorage.setItem('calc', JSON.stringify(dbOperation));
+  localStorage.setItem("calc", JSON.stringify(dbOperation));
 };
 
-
-form.addEventListener('submit', addOperation);
-
-historyList.addEventListener('click', deleteOperation);
+/* Повесил событие */
+form.addEventListener("submit", addOperation);
+historyList.addEventListener("click", deleteOperation);
 
 init();
